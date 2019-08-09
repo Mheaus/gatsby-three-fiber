25e6ec6c66a8c023e6442f14b1949e14a706791e
Mathieu Audebert <mathieu.audebert@timeonegroup.com>
2019-08-09 14:42:53 +0200

    start project from gatsby starter

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Gatsby Three Fiber`,
+    description: `A gatsby test project to experiment with react-three-fiber and friends`,
+    author: `@mheaus`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,7 +18,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
+        name: `gatsby-three-fiber`,
         short_name: `starter`,
         start_url: `/`,
         background_color: `#663399`,
@@ -27,8 +27,17 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    `gatsby-plugin-styled-components`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
+    {
+      resolve: 'gatsby-plugin-web-font-loader',
+      options: {
+        google: {
+          families: ['Catamaran', 'Poppins'],
+        },
+      },
+    },
   ],
-}
+};
