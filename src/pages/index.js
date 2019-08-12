b43bcdad4939e3c747b9e7b9ee7bfa99de216cef
Mathieu Audebert <mathieu.audebert@timeonegroup.com>
2019-08-12 14:37:48 +0200

    feat: add more cubes + cannon + stagger

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,51 +5,100 @@ import { TimelineMax } from 'gsap';
 
 import SEO from '../components/seo';
 
-function Cube() {
+const cubes = [];
+
+function rotateElement(node) {
+  new TimelineMax().to(node.rotation, 0.5, {
+    x: node.rotation.x + Math.PI / 2,
+    y: node.rotation.y + Math.PI / 2,
+  });
+  new TimelineMax().to(node.scale, 0.25, { x: 0.875, y: 0.875, z: 0.875 }).to(node.scale, 0.25, { x: 1, y: 1, z: 1 });
+}
+
+function Plane() {
+  // Register plane as a physics body with zero mass
+  return (
+    <mesh position={[0, 0, -10]} receiveShadow>
+      <planeBufferGeometry attach="geometry" args={[1000, 1000]} />
+      <meshPhongMaterial attach="material" color="#5994a7" />
+    </mesh>
+  );
+}
+
+function Cube({ position }) {
   const groupRef = useRef();
 
   useEffect(() => {
     if (groupRef.current) {
-      const interval = setInterval(
-        () =>
-          new TimelineMax().to(groupRef.current.rotation, 0.5, {
-            x: groupRef.current.rotation.x + Math.PI / 2,
-            y: groupRef.current.rotation.y + Math.PI / 2,
-          }),
-        2000
-      );
+      // register cube
+      const index = cubes.push(groupRef.current);
 
-      return () => clearInterval(interval);
+      return () => cubes.splice(index, 1);
     }
+
+    return () => {};
   }, [groupRef.current]);
 
-  function onClick(event) {
-    console.log(event, groupRef.current);
-
-    new TimelineMax().to(groupRef.current.position, 1, { x: groupRef.current.position.x + 1 });
-  }
-
   return (
-    <group ref={groupRef}>
-      <mesh onClick={onClick} onPointerOver={() => console.log('hover')} onPointerOut={() => console.log('unhover')}>
-        <boxGeometry attach="geometry" args={[1, 1, 1]} />
-        <meshBasicMaterial attach="material" color="#2f262d" />
+    <group ref={groupRef} position={position} receiveShadow>
+      <mesh
+        onClick={() => rotateElement(groupRef.current)}
+        onPointerOver={({ object }) =>
+          new TimelineMax()
+            .to(object.material.color, 0.5, { r: 189, g: 66, b: 114 })
+            .to(object.material.color, 0.5, { r: 1, g: 1, b: 1 })
+        }
+        receiveShadow
+        castShadow
+      >
+        <boxGeometry attach="geometry" args={[2, 2, 2]} />
+        <meshStandardMaterial attach="material" color="#ffffff" opacity={0.7} />
       </mesh>
     </group>
   );
 }
 
 function IndexPage() {
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const stagger = {
+        grid: [15, 7],
+        from: 'center',
+        amount: 1.25,
+      };
+
+      new TimelineMax().staggerTo(
+        cubes.map(({ scale }) => scale),
+        0.5,
+        { x: 0.875, y: 0.875, z: 0.875, stagger },
+        0.01
+      );
+      new TimelineMax().delay(1).staggerTo(cubes.map(({ scale }) => scale), 0.5, { x: 1, y: 1, z: 1, stagger }, 0.01);
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, [cubes]);
+
   return (
     <>
       <SEO title="Home" />
       <h1>Hi people</h1>
       <p>Welcome to your new Gatsby site.</p>
       <p>Now go build something great.</p>
-      <Canvas style={{ display: 'block', position: 'absolute', top: 0, left: 0, height: '100%', width: '100%' }}>
-        <ambientLight intensity={0.5} />
-        <spotLight intensity={0.6} position={[30, 30, 50]} angle={0.2} penumbra={1} castShadow />
-        <Cube />
+      <Canvas
+        camera={{ position: [0, 0, 5] }}
+        style={{ display: 'block', position: 'absolute', top: 0, left: 0, height: '100%', width: '100%' }}
+      >
+        <Plane />
+        <ambientLight intensity={1} />
+        {/* <spotLight intensity={0.2} position={[100, 30, 50]} angle={0.1} penumbra={1} castShadow /> */}
+        {/* <pointLight intensity={0.05} position={[0, 0, 50]} penumbra={1} castShadow /> */}
+        <pointLight intensity={1} position={[0, 0, -10]} penumbra={1} castShadow color="0x5994a7" />
+        {new Array(7)
+          .fill()
+          .map((_, rowIndex) =>
+            new Array(15).fill().map((_, colIndex) => <Cube position={[-14 + colIndex * 2, 6 - rowIndex * 2, -5]} />)
+          )}
       </Canvas>
     </>
   );
